c8f893e99403375fd095725e7ed868d391ddb6f1
Dickson Daniel Peprah <techpepson@gmail.com>
2025-07-07 04:25:01 +0000

    feat(single pest details): implement the single pest disease details page.

---
+++ lib/screens/library/single_crop_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SingleCropDetails extends StatefulWidget {
+  const SingleCropDetails({super.key});
+
+  @override
+  State<SingleCropDetails> createState() => _SingleCropDetailsState();
+}
+
+class _SingleCropDetailsState extends State<SingleCropDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
