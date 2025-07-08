0b3dc3b8d48eedc7d33fbe326f83f62e19dbef39
Dickson Daniel Peprah <techpepson@gmail.com>
2025-07-08 04:23:53 +0000

    feat(community setup): setup the community session.

---
+++ lib/screens/community/community_session.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommunitySession extends StatefulWidget {
+  const CommunitySession({super.key});
+
+  @override
+  State<CommunitySession> createState() => _CommunitySessionState();
+}
+
+class _CommunitySessionState extends State<CommunitySession> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
